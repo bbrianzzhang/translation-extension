1502417343add8d7867c928f03d70e3fa297c0ec
Brian Zhang <bbrianzzhang@gmail.com>
2021-01-31 01:57:20 -0800

    send messages

--- Extension/background.js
+++ Extension/background.js
@@ -11,16 +11,15 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
     return true;
 });
 
-// chrome.runtime.onMessage.addListener(
-//     function (request, sender, sendResponse) {
-//         var url = serverhost + '/translate/get_translation/?topic=' + encodeURIComponent(request.topic);
-//         console.log(url);
-//
-//         //var url = "http://127.0.0.1:8000/wiki/get_wiki_summary/?topic=%22COVID19%22"
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(response => sendResponse({farewell: response}))
-//             .catch(error => console.log(error))
-//
-//         return true;
-//     });
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        var url = serverhost + '/translate/get_translation/?topic=' + encodeURIComponent(request.x1) + "," + encodeURIComponent(request.x2) + "," + encodeURIComponent(request.y1) + "," + encodeURIComponent + (request.y2) + "," + encodeURIComponent(request.topic);
+        console.log(url);
+
+        fetch(url)
+            .then(response => response.json())
+            .then(response => sendResponse({farewell: response}))
+            .catch(error => console.log(error))
+
+        return true;
+    });
